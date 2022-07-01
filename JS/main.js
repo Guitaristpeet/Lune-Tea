95c4b45b0eb7be763b240e7a96b23d22f607a25e
SU-T <guitaristpeet@gmail.com>
2022-07-01 15:19:54 -0700

    create slide with swiper

---
+++ JS/main.js
@@ -0,0 +1,10 @@
+const swiper = new Swiper('.swiper', {
+    spaceBetween: 100,
+    slidesPerView: 3.7,
+    loop: true,
+    autoplay: {
+        delay: 1,
+    },
+    freeMode: true, 
+    speed: 7000,
+  });
